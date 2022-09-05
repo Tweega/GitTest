5109fcc0d7728b2896e8c66cdd735588c910d4f0
Christopher Roe <cpr@tweega.com>
2022-09-05 11:22:10 +0100

    renamed hello to interpolateCode

--- interpolateCode.py
+++ interpolateCode.py
@@ -2,6 +2,8 @@ import getopt
 import sys
 import os
 import re
+import shutil
+import tempfile
 from git import Repo
 
 PROGRAM = "ProgamName tbd"
@@ -54,8 +56,9 @@ if __name__ == "__main__":
         # verbose?
         # error log path?
 
-    options, remainder = getopt.getopt(sys.argv[1:], 'p:g:t:o:vh', ['codepath=', 
+    options, remainder = getopt.getopt(sys.argv[1:], 'p:g:b:t:o:vh', ['codepath=', 
                                                         'gitrepo=',
+                                                        'gitbranch',
                                                         'template=',
                                                         'outputpath=',
                                                         'strip',
@@ -64,45 +67,55 @@ if __name__ == "__main__":
                                                         'verbose',
                                                         ])
 
-    repo_path = os.getenv('NORSK_SAMPLE_CODE_PATH')
-    repo_git_url = os.getenv('NORSK_SAMPLE_CODE_GIT_URL')
-    template_file = "media_proto.html"
-    output_path = r"~/work/python/hello"
+    repoPath = os.getenv('NORSK_SAMPLE_CODE_PATH')
+    outputPath = os.getenv('NORSK_SAMPLE_OUTPUT_PATH')
+    repoGitUrl = os.getenv('NORSK_SAMPLE_CODE_GIT_URL')
+    templateFile = "media_proto.html" #DEBUG ONLY tk
     verbose = True
-    comment = "#"
     strip = False
+    useTempDir = False
+    repoGitBranch = "main"
 
-    def getFileLines(file_name, cache=False): # cache would be for when getting data from git
-        err = None
-
-        cachedLines = cachedFiles.get(file_name)
-        if cachedLines:
-            return cachedLines, err
-        else:
-            # assume source is file for the moment
-            fileLines = []
-            if repo_path != "":
-                file_path = os.path.join(repo_path, file_name)
-                with open(file_path, 'r', encoding='UTF-8') as file:
-                    while (bool(lineIn := file.readline())):
-                        line = lineIn.rstrip()
-                        fileLines.append(line)
-            elif repo_git_url != "":
-                # load file lines direct from git repo tbd
-                print ("tbd")
+    def getFileLines(fileName, cache=False): # cache would be for when getting data from git
+        errorMsg = None
+        try:
+            cachedLines = cachedFiles.get(fileName)
+            if cachedLines:
+                return cachedLines, errorMsg
             else:
-                err = f"unable to load file: {file_name}"
-            
-            if (err is None) and cache:
-                cachedFiles[file_name] = fileLines
+                # assume source is file for the moment
+                fileLines = []
+                if repoPath != "":
+                    filePath = os.path.join(repoPath, fileName)
+                    with open(filePath, 'r', encoding='UTF-8') as file:
+                        while (bool(lineIn := file.readline())):
+                            line = lineIn.rstrip()
+                            fileLines.append(line)
+                else:
+                    errorMsg = f"unable to load file: {fileName}"
+                
+                if (errorMsg is None) and cache:
+                    cachedFiles[fileName] = fileLines
 
-            return fileLines, err
-        
+                return fileLines, errorMsg
+        except Exception as e:
+                msg = str(e)
+                errorMsg = f"Error occured in getFileLines: {msg}"
+        return [], errorMsg
+            
 
     def saveTextFile(fileText, outPath):
-        vPrint(fileText)
+        # vPrint(fileText)
         vPrint(f"Saving to : {outPath}")
-        print ("saveTextFile to be done")
+        errorMsg = None
+        try:
+            with open(outPath, 'w') as f:
+                f.write(fileText)
+        except Exception as e:
+                msg = str(e)
+                errorMsg = f"Error occured when saving output file. {msg}"
+        errorMsg
+            
 
     def vPrint(str):
         if verbose:
@@ -110,79 +123,98 @@ if __name__ == "__main__":
 
     for opt, arg in options:
         if opt in ('-p', '--codepath'):
-            repo_path = arg            
+            repoPath = arg            
         elif opt in ('-g', '--gitrepo'):
-            repo_git_url = arg
+            repoGitUrl = arg
+        elif opt in ('-b', '--gitbranch'):
+            repoGitBranch = arg
         elif opt in ('-t', '--template'):
-            template_file = arg
+            templateFile = arg
             verbose = True
         elif opt in ('-o', '--outputpath'):
-            output_path = arg
+            outputPath = arg
         elif opt in ('-s', '--strip'):
             strip = True
         elif opt in ('-v', '--version'):
             errorState = f"Version: {PROGRAM}: {VERSION}"
         elif opt in ('-h', '--help'):
-            usageLines = ["programName.py",
+            usageLines = ["usage", "programName.py",
             "options: ", 
-            "-p, --codepath : the local path to code repository. Required unless --strip flag is specified, in which case only strip operation performed",
-            "-g, --gitrepo: the url to git repo to extract code examples from. (optional?)",
-            "-t, --template: the file into which excerpts should be inserted",
-            "-o, --output: the path (if only file name supplied, repo_path is assumed as directory) to file to save result of interpolating template with excerpts (or directory to copy stripped files into)",
-            "-s, --strip: Strips files listed in template file of [excerpt] markup and copies to --output directory.",
+            "-p, --codepath : the local path to code repository. (Required unless --gitrepo provided).  Alternatively set env variable NORSK_SAMPLE_CODE_PATH",
+            "-g, --gitrepo: the url to git repo to extract code examples from. (optional).  Alternatively set env variable NORSK_SAMPLE_CODE_GIT_URL",
+            "-b, --gitbranch: defaults to main, and currently only used when no codepath supplied",
+            "-t, --template: the file into which excerpts should be inserted (required)",
+            "-o, --outputpath: directory to save output files into(required). Alternatively set env variable NORSK_SAMPLE_OUTPUT_PATH",
+            "-s, --strip: Strips files listed in template file of [excerpt] markup and copies to --outputpath directory.",
             "-v, --version: prints version of this program (the program does not execute when this option is provided)",
             "-h, --help: displays this usage text (the program does not execute when this option is provided)"
             ]
-            usage = "\n".join(usageLines)
-            errorState = f"usage: {usage}"            
+            errorState = "\n".join(usageLines)            
         elif opt in ('--verbose'):
             verbose = True
     
-    if not(os.path.isdir(repo_path)):
-        # this will probably change so that either file path or git repo should be specified as alternative file access mechanisms
-        errorState = "Unable to access code path: {repo_path}"
+    if repoPath and not(os.path.isdir(repoPath)):
+        errorState = f"Unable to access code path: {repoPath}"
 
+    if not outputPath:
+        errorState = "--outputpath must be specified"
+    else:
+        if outputPath == repoPath:
+            errorState = "Repo path and output path should not be the same otherwise files may be overwritten"
+        elif not(os.path.isdir(outputPath)):
+            errorState = f"Unable to access output path: {outputPath}"
+
+    if not repoPath:
+        if not repoGitUrl:
+            errorState = "One of --gitrepo or --repopath must be provided"
+        else:
+            # Create temporary dir
+            repoPath = tempfile.mkdtemp()
+            useTempDir = True
+            # # Clone into temporary dir
+            # git.Repo.clone_from('stack@127.0.1.7:/home2/git/stack.git', t, branch='master', depth=1)
+            # # Copy desired file from temporary dir
+            # shutil.move(os.path.join(t, 'setup.py'), '.')
+            # # Remove temporary dir
+            # shutil.rmtree(t)
+        
     
-    print (repo_path)
-    print (repo_git_url)
-    print (template_file)
-    print (verbose)
-    print (comment)
-
-    # keep track of 
-        # list of files to be examined
-        # accumulated output - a copy of the file with code excerpts interpolated in
-        # map of the completed excerpts keyed on file name and excerptid - value being a list of content lines
-        # map of the open excerpts keyed on file name and excerptid - - value being a list of content lines
+    vPrint (f"repoPath: {repoPath}")
+    vPrint (f"repoGitUrl: {repoGitUrl}")
+    vPrint (f"repoGitBranch: {repoGitBranch}")
+    vPrint (f"templateFile: {templateFile}")
+    vPrint (f"outputPath: {outputPath}")
+    vPrint (f"verbose: {verbose}")
 
     if errorState is None : 
-        if repo_git_url :
+        if repoGitUrl :
             try:
-                action = "pull"
-                if os.path.isdir(repo_path) :   # we actually need to check if git initialised yet tk
-                    repo = Repo(repo_path) 
-                    repo.remotes.origin.pull()  # this assumes that git url is valid, which it may not be tk
-                else:         
-                    action = "clone" 
-                    repo = Repo.clone_from(repo_git_url, repo_path)
+                action = "clone"
+                if useTempDir:   # we actually need to check if git initialised yet tk
+                    repo = Repo.clone_from(repoGitUrl, repoPath, branch=repoGitBranch, depth=1)
+                else:
+                    action = "pull" 
+                    repo = Repo(repoPath) 
+                    repo.remotes.origin.pull() 
+                    # checkout specific branch?     
+                    
             except Exception as e:
                 msg = str(e)
                 errorState = f"Error occured when interacting with git.  Action: {action}.  Error: {msg}"
-
                                     
-            # repo = Repo(repo_path)
+            # repo = Repo(repoPath)
             # check that the repository loaded correctly
             if errorState is None:
                 if not repo.bare:
-                    vPrint('Repo at {} successfully loaded.'.format(repo_path))
+                    vPrint('Repo at {} successfully loaded.'.format(repoPath))
                     print_repository(repo)
                     # create list of commits then print some of them to stdout
-                    commits = list(repo.iter_commits('main'))[:COMMITS_TO_PRINT]
+                    commits = list(repo.iter_commits(repoGitBranch))[:COMMITS_TO_PRINT]
                     for commit in commits:
                         print_commit(commit)
                         pass
                 else:
-                    errMsg = 'Could not load repository at {} :('.format(repo_path)
+                    errMsg = 'Could not load repository at {} :('.format(repoPath)
                     errorState = errMsg
         
 
@@ -190,8 +222,8 @@ if __name__ == "__main__":
         # open the template file and get a list of files that excerpts need to be retrieved from 
                 
         # assume for the moment that the template file is in the same repo as code samples
-        templateFile = os.path.join(repo_path, template_file)
-        fileLines, errorState = getFileLines(templateFile, True)
+        templateFilePath = os.path.join(repoPath, templateFile)
+        fileLines, errorState = getFileLines(templateFilePath, True)
         numLines = len(fileLines)
         lineIndex = 0
         while ((lineIndex < numLines) and (errorState is None)):
@@ -234,7 +266,7 @@ if __name__ == "__main__":
 
             #  we have a list of files to extract excerpts from
             for codeFile in filesToSearchMap:
-                codeFilePath = os.path.join(repo_path, codeFile)
+                codeFilePath = os.path.join(repoPath, codeFile)
                 
                 fileLines, errorState = getFileLines(codeFilePath, strip) # if stripping coode files then keep in cache
                 numLines = len(fileLines)
@@ -303,7 +335,7 @@ if __name__ == "__main__":
         # still to do is to incorporate the file name into the map structure for excerpts tk
         if errorState is None: 
             '# now iterate through the user guide creating another copy with extracts inserted'
-            fileLines, errorState = getFileLines(templateFile)
+            fileLines, errorState = getFileLines(templateFilePath)
             numLines = len(fileLines)
             lineIndex = 0
             while ((lineIndex < numLines) and (errorState is None)):
@@ -340,32 +372,27 @@ if __name__ == "__main__":
 
         if errorState is None:
             guideStr = "\n".join(accumulatedOutput)
-            saveTextFile(guideStr, "some file name")
-            strip = True #DEBUG ONLY remove tbd
-            if strip: 
-                excerptFilter = lambda l: not(bool(re.search(reExcerpt, line)))
-                for codeFile in filesToSearchMap:
-                    codeFilePath = os.path.join(repo_path, codeFile)
-            
-                    codeLines, errorState = getFileLines(codeFilePath)
-                    strippedLines = filter(excerptFilter, codeLines)
-                    strippedText = "\n".join(strippedLines)
-                    outPath = os.path.join(output_path, codeFile)
+            codeFilePath = os.path.join(outputPath, templateFile)
+            errorState = saveTextFile(guideStr, codeFilePath)
 
-                    saveTextFile(strippedText, outPath)
+            if errorState is None:
+                if strip: 
+                    excerptFilter = lambda l: not(bool(re.search(reExcerpt, l)))
+                    for codeFile in filesToSearchMap:
+                        if errorState is None:
+                            codeFilePath = os.path.join(repoPath, codeFile)
+                    
+                            codeLines, errorState = getFileLines(codeFilePath)
+                            strippedLines = filter(excerptFilter, codeLines)
+                            strippedText = "\n".join(strippedLines)
+                            outPath = os.path.join(outputPath, codeFile)
+
+                            errorState = saveTextFile(strippedText, outPath)
 
 
-
-        else:
-            print(f"There was a problem: {errorState}")
-            
+    if errorState is None:
         print ("END")
-        # excerptsFilePath = os.path.join(repo_path, "excerpts.csv")
-        # with open(excerptsFilePath, 'r', encoding='UTF-8') as file:
-        #     while (line := file.readline().rstrip()):
-        #         print(line)
 
-                # start excerpt ^#.+?\[excerpt\s*(.+)\]
-                # end excerpt  ^#.+?\[\/excerpt\s*(.+)\]
-                
+    else:
+        print(f"There was a problem:\n{errorState}")
         
